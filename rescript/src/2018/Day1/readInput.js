99be19172ff75f63bdd5a39ab84c3969f4ab51bb
WON SEOB SEO <pghoou@gmail.com>
2021-06-23 21:13:47 +0300

    Read input from common inputs folder (rescript solution)

--- rescript/src/2018/Day1/readInput.js
+++ rescript/src/2018/Day1/readInput.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 let input;
 
 try {
-  input = fs.readFileSync("./src/2018/Day1/input.txt", "utf8");
+  input = fs.readFileSync("../inputs/2018/Day1/input.txt", "utf8");
 } catch (err) {
   console.error(err);
 }
