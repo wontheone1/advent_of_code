ffdff64a7c8b3b36487c665ef652c8a5efdf3648
WON SEOB SEO <pghoou@gmail.com>
2021-06-23 21:08:29 +0300

    Solve 2018 day1, part 2 with JS

--- JavaScript/src/2018/Day1/index.js
+++ JavaScript/src/2018/Day1/index.js
@@ -1,7 +1,31 @@
 const { input } = require("./readInput");
 
-const frequencies = input.split(/\r?\n/).map((item) => Number(item));
+const frequencyChanges = input
+  .trim()
+  .split(/\r?\n/)
+  .map((item) => Number(item));
 
-const sum = frequencies.reduce((acc, num) => acc + num, 0);
+const sum = frequencyChanges.reduce((acc, num) => acc + num, 0);
 
-console.log(sum);
+console.log("Part1:", sum);
+
+// reduceWhile could be used   https://ramdajs.com/docs/#reduceWhile
+let seenFrequencies = {};
+let currentFrequency = 0;
+let i = 0;
+
+while (!seenFrequencies[String(currentFrequency)]) {
+  let key = String(currentFrequency);
+  seenFrequencies[key] = true;
+  currentFrequency = currentFrequency + frequencyChanges[i];
+  //   console.log(currentFrequency, i, frequencyChanges.length);
+  if (i + 1 < frequencyChanges.length) {
+    i++;
+  } else {
+    i = 0;
+  }
+}
+
+let firstFrequencySeenTwice = currentFrequency;
+
+console.log("Part2:", firstFrequencySeenTwice);
