053c51df80668f792f7457174c7d91d4ac5fc5fc
WON SEOB SEO <pghoou@gmail.com>
2021-06-24 15:06:23 +0300

    Setup problem of 2018 day 2

---
+++ clojure/year2018/day2/src/solution.clj
@@ -0,0 +1,17 @@
+(ns solution)
+
+(def input
+  "abcdef
+  bababc
+  abbcde
+  abcccd
+  aabcdd
+  abcdee
+  ababab")
+
+(defn run [opts]
+  (println input))
+
+; run with
+; cd year2018/day2
+; clj -X solution/run
