d707ba83fda1f2a5b1adc0fa8572e84d409622f3
WON SEOB SEO <pghoou@gmail.com>
2021-06-24 23:30:47 +0300

    Implement get-common-string-part function

--- clojure/year2018/day2/src/part2.clj
+++ clojure/year2018/day2/src/part2.clj
@@ -12,8 +12,16 @@
             str2)
        (reduce +)))
 
+(defn get-common-string-part [str1 str2]
+  (apply str (keep-indexed
+               (fn [i ch]
+                 (when (= ch (get str1 i))
+                   ch))
+               str2)))
+
 (defn run [opts]
-  (prn (string-differences "fghij" "fauij")))
+  (prn (string-differences "fghij" "fauij")
+       (get-common-string-part "fghij" "fghij")))
 
 ; run with
 ; cd year2018/day2
