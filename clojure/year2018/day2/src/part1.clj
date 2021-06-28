2a4c67684af5cee849e2927d576e5c4b3b2db010
WON SEOB SEO <pghoou@gmail.com>
2021-06-28 21:42:05 +0300

    Use map instead of vector

--- clojure/year2018/day2/src/part1.clj
+++ clojure/year2018/day2/src/part1.clj
@@ -4,26 +4,26 @@
   (slurp "../../../inputs/2018/Day2/input.txt"))
 
 (defn run [opts]
-  (let [[times-two-of-letter-detected
-         times-three-of-letter-detected] (->> input
-                                              clojure.string/split-lines
-                                              (map (comp (fn [frequencies->chars]
-                                                           [(if (frequencies->chars 2)
-                                                              1
-                                                              0)
-                                                            (if (frequencies->chars 3)
-                                                              1
-                                                              0)])
-                                                         #(group-by (fn [[_k v]] v) %)
-                                                         frequencies))
-                                              (reduce
-                                                (fn [[times-two-of-letter-detected
-                                                      times-three-of-letter-detected]
-                                                     [two-of-letter-detected
-                                                      three-of-letter-detected]]
-                                                  [(+ times-two-of-letter-detected two-of-letter-detected)
-                                                   (+ times-three-of-letter-detected three-of-letter-detected)])))]
-    (prn (* times-two-of-letter-detected times-three-of-letter-detected))))
+  (let [{:keys [twos threes]} (->> input
+                                   clojure.string/split-lines
+                                   (map (comp (fn [frequencies->chars]
+                                                {:two-detected   (if (frequencies->chars 2)
+                                                                   1
+                                                                   0)
+                                                 :three-detected (if (frequencies->chars 3)
+                                                                   1
+                                                                   0)})
+                                              #(group-by (fn [[_k v]] v) %)
+                                              frequencies))
+                                   (reduce
+                                     (fn [{:keys [twos threes]}
+                                          {:keys [two-detected
+                                                  three-detected]}]
+                                       {:twos   (+ twos two-detected)
+                                        :threes (+ threes three-detected)})
+                                     {:twos   0
+                                      :threes 0}))]
+    (prn (* twos threes))))
 
 ; run with
 ; cd year2018/day2
