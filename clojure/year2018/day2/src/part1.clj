ea24047708ab96584e58dcbc6e43d60c6fa7a893
WON SEOB SEO <pghoou@gmail.com>
2021-06-28 22:01:48 +0300

    refactor: function pipeline

--- clojure/year2018/day2/src/part1.clj
+++ clojure/year2018/day2/src/part1.clj
@@ -3,18 +3,20 @@
 (def input
   (slurp "../../../inputs/2018/Day2/input.txt"))
 
+(def group-chars-by-frequencies
+  (comp #(group-by (fn [[_ch freq]] freq) %) frequencies))
+
 (defn run [opts]
   (let [{:keys [twos threes]} (->> input
                                    clojure.string/split-lines
-                                   (map (comp (fn [frequencies->chars]
-                                                {:two-detected   (if (frequencies->chars 2)
-                                                                   1
-                                                                   0)
-                                                 :three-detected (if (frequencies->chars 3)
-                                                                   1
-                                                                   0)})
-                                              #(group-by (fn [[_k v]] v) %)
-                                              frequencies))
+                                   (map group-chars-by-frequencies)
+                                   (map (fn [frequencies->chars]
+                                          {:two-detected   (if (frequencies->chars 2)
+                                                             1
+                                                             0)
+                                           :three-detected (if (frequencies->chars 3)
+                                                             1
+                                                             0)}))
                                    (reduce
                                      (fn [{:keys [twos threes]}
                                           {:keys [two-detected
