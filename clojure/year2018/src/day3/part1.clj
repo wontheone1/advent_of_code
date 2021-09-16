79d1969b7d85fa797d13daa663338245363abc20
WON SEOB SEO <pghoou@gmail.com>
2021-09-16 15:28:11 +0300

    Add plot-claim function

--- clojure/year2018/src/day3/part1.clj
+++ clojure/year2018/src/day3/part1.clj
@@ -17,3 +17,18 @@
     [id start-position width&height]))
 
 (def claims (map parse-claims input))
+
+(defn plot-claim [id [start-x start-y] [claim-width claim-height]]
+  (let [plot-height (+ start-y claim-height)
+        plot-weight (+ start-x claim-width)
+        char-representaions (for [y (range plot-height)
+                                  x (range plot-weight)]
+                              (let [ch (if (and (>= x start-x) (>= y start-y))
+                                         id
+                                         ".")]
+                                (if (= x (dec plot-weight))
+                                  (str ch \newline)
+                                  ch)))]
+    (apply str char-representaions)))
+
+(println (plot-claim "1" [1 3] [4 4]))
