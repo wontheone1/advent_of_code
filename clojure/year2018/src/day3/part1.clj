c32612439f558fb8a21db8f8d2042487bef9ef70
WON SEOB SEO <pghoou@gmail.com>
2021-09-16 15:10:57 +0300

    Parse claims

--- clojure/year2018/src/day3/part1.clj
+++ clojure/year2018/src/day3/part1.clj
@@ -1,4 +1,18 @@
 (ns day3.part1)
 
 (def input
-  (slurp "../../inputs/2018/Day3/input.txt"))
+  (-> (slurp "../../inputs/2018/Day3/input.txt")
+      (clojure.string/split-lines)))
+
+(defn string->int [position]
+  (Integer/parseInt position))
+
+(defn parse-claims [line]
+  (let [[id start-position width&height] (clojure.string/split line #"\s@\s|:\s")
+        start-position (->> (clojure.string/split start-position #",")
+                            (mapv string->int))
+        width&height (->> (clojure.string/split width&height #"x")
+                          (mapv string->int))]
+    [id start-position width&height]))
+
+(def claims (map parse-claims input))
