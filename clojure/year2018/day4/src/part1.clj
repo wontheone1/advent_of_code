8486e287f3a3bf9636f2942dd46ce174bb3915f5
WON SEOB SEO <pghoou@gmail.com>
2022-09-05 09:29:46 +0300

    Parse single line

--- clojure/year2018/day4/src/part1.clj
+++ clojure/year2018/day4/src/part1.clj
@@ -1,1 +1,25 @@
-(ns part1)
+(ns part1
+  (:require [clojure.string]))
+
+(def input
+  (->
+   (slurp "../../../inputs/2018/Day4/input.txt")
+   clojure.string/split-lines)
+  )
+
+(comment
+ input
+ )
+
+(defn parse-line [line]
+  (let [timestamp-date-part (re-find #"\d{4}-\d{2}-\d{2}" line)
+        timestamp-minute (Integer/parseInt (-> (re-find #"\d{2}:\d{2}" line)
+                                               (clojure.string/split #":")
+                                               (second)))]
+    {:guard-no (Integer/parseInt (-> (re-find #"#\d+" line)
+                                     (subs 1)))
+     :timestamp-date-part timestamp-date-part
+     :timestamp-hour-minute timestamp-minute}))
+
+(comment
+ (parse-line "[1518-11-01 00:05] Guard #10 begins shift"))
