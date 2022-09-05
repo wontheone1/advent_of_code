4819688d5452aa7cbfe5d6d0a8f90b969f1645e2
WON SEOB SEO <pghoou@gmail.com>
2022-09-05 10:52:00 +0300

    Aggregate sleep records

--- clojure/year2018/day4/src/part1.clj
+++ clojure/year2018/day4/src/part1.clj
@@ -38,3 +38,45 @@
  (parse-line "[1518-11-01 00:10] falls asleep")
  (parse-line "[1518-11-01 00:25] wakes up"))
 
+(defn aggregate-sleep-records [sorted-parsed-lines]
+  (reduce
+   (fn [agg {:keys [guard-no kind timestamp-date-part timestamp-minute] :as _parsed-line}]
+     (case kind
+       :change-shift
+       (merge
+        agg
+        {:current-date timestamp-date-part
+         :current-guard-no guard-no
+         :current-sleep-start nil})
+
+       :falls-asleep
+       (merge
+        agg
+        {:current-date timestamp-date-part
+         :current-sleep-start timestamp-minute})
+
+       :wakes-up
+       (merge
+        agg
+        {
+         ; :current-date timestamp-date-part (Data shows that guards don't sleep overnight. they only sleep from 00:00 ~ 01:00)
+         ; :current-sleep-start nil (if :falls-asleep and :wakes-up alternate as expected, then no need to clean up)
+         :aggregated-sleep-record (conj (:aggregated-sleep-record agg)
+                                        {:guard-no (:current-guard-no agg)
+                                         :date (:current-date agg)
+                                         :sleep-start-time (:current-sleep-start agg)
+                                         :sleep-end-time timestamp-minute})})))
+   {:current-date nil
+    :current-guard-no nil
+    :current-sleep-start nil
+    :aggregated-sleep-record []}
+   sorted-parsed-lines))
+
+(defn solve [input]
+  (->> input
+       sort
+       (map parse-line)
+       aggregate-sleep-records))
+
+(comment
+ (solve input))
