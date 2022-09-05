0a3942dd406a327117016827490d44f3f69435c8
WON SEOB SEO <pghoou@gmail.com>
2022-09-05 10:03:27 +0300

    Parse line by different kinds of lines

--- clojure/year2018/day4/src/part1.clj
+++ clojure/year2018/day4/src/part1.clj
@@ -15,11 +15,26 @@
   (let [timestamp-date-part (re-find #"\d{4}-\d{2}-\d{2}" line)
         timestamp-minute (Integer/parseInt (-> (re-find #"\d{2}:\d{2}" line)
                                                (clojure.string/split #":")
-                                               (second)))]
-    {:guard-no (Integer/parseInt (-> (re-find #"#\d+" line)
-                                     (subs 1)))
-     :timestamp-date-part timestamp-date-part
-     :timestamp-hour-minute timestamp-minute}))
+                                               (second)))
+        kind (cond
+               (re-find #"Guard" line)
+               :change-shift
+
+               (re-find #"falls" line)
+               :falls-asleep
+
+               (re-find #"wakes" line)
+               :wakes-up)]
+    (merge
+     {:kind kind
+      :timestamp-date-part timestamp-date-part
+      :timestamp-minute timestamp-minute}
+     (when (= kind :change-shift)
+       {:guard-no (Integer/parseInt (-> (re-find #"#\d+" line)
+                                        (subs 1)))}))))
 
 (comment
- (parse-line "[1518-11-01 00:05] Guard #10 begins shift"))
+ (parse-line "[1518-11-01 00:05] Guard #10 begins shift")
+ (parse-line "[1518-11-01 00:10] falls asleep")
+ (parse-line "[1518-11-01 00:25] wakes up"))
+
